5381f95076f0a31d946bc2c611f4973d86fc2a40
Gustavo Niemeyer <gustavo@niemeyer.net>
2011-03-05 12:55:43 -0300

    Improved goodOSArch test organization.

--- src/cmd/goinstall/syslist_test.go
+++ src/cmd/goinstall/syslist_test.go
@@ -8,45 +8,49 @@ import (
 	"testing"
 )
 
-func TestGoodOSArch(t *testing.T) {
-	var thisOS, thisArch = runtime.GOOS, runtime.GOARCH
-	var otherOS, otherArch string
+var (
+	thisOS    = runtime.GOOS
+	thisArch  = runtime.GOARCH
+	otherOS   = "freebsd"
+	otherArch = "arm"
+)
 
-	if thisOS == "darwin" {
+func init() {
+	if thisOS == otherOS {
 		otherOS = "linux"
-	} else {
-		otherOS = "darwin"
 	}
-	if thisArch == "arm" {
+	if thisArch == otherArch {
 		otherArch = "amd64"
-	} else {
-		otherArch = "arm"
 	}
+}
 
-	var tests = []struct {
-		name string
-		res  bool
-	}{
-		{"file.go", true},
-		{"file.c", true},
-		{"file_foo.go", true},
-		{"file_" + thisArch + ".go", true},
-		{"file_" + otherArch + ".go", false},
-		{"file_" + thisOS + ".go", true},
-		{"file_" + otherOS + ".go", false},
-		{"file_" + thisOS + "_" + thisArch + ".go", true},
-		{"file_" + otherOS + "_" + thisArch + ".go", false},
-		{"file_" + thisOS + "_" + otherArch + ".go", false},
-		{"file_" + otherOS + "_" + otherArch + ".go", false},
-		{"file_foo_" + thisArch + ".go", true},
-		{"file_foo_" + otherArch + ".go", false},
-		{"file_" + thisOS + ".c", true},
-		{"file_" + otherOS + ".c", false},
-	}
+type GoodFileTest struct {
+	name   string
+	result bool
+}
 
+var tests = []GoodFileTest{
+	{"file.go", true},
+	{"file.c", true},
+	{"file_foo.go", true},
+	{"file_" + thisArch + ".go", true},
+	{"file_" + otherArch + ".go", false},
+	{"file_" + thisOS + ".go", true},
+	{"file_" + otherOS + ".go", false},
+	{"file_" + thisOS + "_" + thisArch + ".go", true},
+	{"file_" + otherOS + "_" + thisArch + ".go", false},
+	{"file_" + thisOS + "_" + otherArch + ".go", false},
+	{"file_" + otherOS + "_" + otherArch + ".go", false},
+	{"file_foo_" + thisArch + ".go", true},
+	{"file_foo_" + otherArch + ".go", false},
+	{"file_" + thisOS + ".c", true},
+	{"file_" + otherOS + ".c", false},
+}
+
+func TestGoodOSArch(t *testing.T) {
 	for _, test := range tests {
-		if goodOSArch(test.name) != test.res {
-			t.Fatalf("goodOSArch(%q) != %s", test.name, test.res)
+		if goodOSArch(test.name) != test.result {
+			t.Fatalf("goodOSArch(%q) != %v", test.name, test.result)
 		}
 	}
 }
